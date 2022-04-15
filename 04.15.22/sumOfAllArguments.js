078105ab7e178484d274edb1beb05fa2a7f78180
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-04-15 18:17:28 -0400

    sum solution using Rest arg parameter

---
+++ 04.15.22/sumOfAllArguments.js
@@ -0,0 +1,13 @@
+// Sum of all arguments
+
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+function sum(...nums) {
+    return nums.reduce((acc, curr) => acc + curr, 0);
+}
