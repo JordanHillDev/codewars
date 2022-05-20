a7225a707419d95aa331c76324ae9b0080195e82
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-05-20 19:55:20 -0400

    tripleX solution using indexOf and slice

---
+++ 05.20.22/tripleX.js
@@ -0,0 +1,16 @@
+// L2: Triple X
+
+// Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+// tripleX("abraxxxas") → true
+// tripleX("xoxotrololololololoxxx") → false
+// tripleX("softX kitty, warm kitty, xxxxx") → true
+// tripleX("softx kitty, warm kitty, xxxxx") → false
+// Note :
+
+// capital X's do not count as an occurrence of "x".
+// if there are no "x"'s then return false
+
+function tripleX(str){
+  return str.slice(str.indexOf('x') + 1, str.indexOf('x') + 3) === 'xx'
+}
