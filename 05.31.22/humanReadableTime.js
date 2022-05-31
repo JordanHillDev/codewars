d5575391be2f928e512f87e493ad07c63b049a64
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-05-31 11:39:00 -0400

    humanReadableTime solution using Math.floor

---
+++ 05.31.22/humanReadableTime.js
@@ -0,0 +1,20 @@
+// Human Readable Time
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+function humanReadable(seconds) {
+    if (seconds === 0) return "00:00:00";
+
+    const hours =   String(Math.floor(seconds / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((seconds - (hours * 3600)) / 60)).padStart(2, "0");
+    const secs =    String(Math.floor(seconds - (hours * 3600) - (minutes * 60))).padStart(2, "0");
+
+    return `${hours}:${minutes}:${secs}`;
+}
