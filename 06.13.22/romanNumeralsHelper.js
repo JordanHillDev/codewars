6bf24adc01296f43126e55c10ee9abf8e4fa7c20
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-06-14 13:35:43 -0400

    added new folder

--- 06.13.22/romanNumeralsHelper.js
+++ 06.13.22/romanNumeralsHelper.js
@@ -22,42 +22,41 @@
 // D	500
 // M	1000
 
-
 const numerals = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+];
 
-  RomanNumerals = {
-    toRoman: function(v) {
-      var s = '';
-      numerals.forEach(function(n) {
-        while (v >= n[1]) {
-          s += n[0];
-          v -= n[1]; 
-        }
-      });
-      return s;
+RomanNumerals = {
+    toRoman: function (v) {
+        var s = "";
+        numerals.forEach(function (n) {
+            while (v >= n[1]) {
+                s += n[0];
+                v -= n[1];
+            }
+        });
+        return s;
     },
-    fromRoman: function(s) {
-      var v = 0;
-      numerals.forEach(function(n) {
-        while (s.substr(0, n[0].length) == n[0]) {
-          s = s.substr(n[0].length);
-          v += n[1];
-        }
-      });
-      return v;
-    }
-  };
+    fromRoman: function (s) {
+        var v = 0;
+        numerals.forEach(function (n) {
+            while (s.substr(0, n[0].length) == n[0]) {
+                s = s.substr(n[0].length);
+                v += n[1];
+            }
+        });
+        return v;
+    },
+};
