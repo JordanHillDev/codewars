26b04d180369a8e35b5cafdf5824d1e4f31006a5
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-07-09 21:02:44 -0400

    scramblies solution using every

---
+++ 07.09.22/scramblies.js
@@ -0,0 +1,33 @@
+// Scramblies
+
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+// Notes:
+
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+// Performance needs to be considered.
+// Examples
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
+
+function scramble(str1, str2) {
+    str1 = str1.split("");
+    return str2.split("").every((letter) => {
+        if (str1.includes(letter)) {
+            str1.splice(str1.indexOf(letter), 1);
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+
+//faster version
+function scramble(str1, str2) {
+    let occurences = str1.split("").reduce((arr, cur) => {
+        arr[cur] ? arr[cur]++ : (arr[cur] = 1);
+        return arr;
+    }, {});
+    return str2.split("").every((character) => --occurences[character] >= 0);
+}
