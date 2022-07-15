e3e5ddc62ce56de19fa46c5d85b37908e6d1c00a
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-07-14 22:45:06 -0400

    added simpler solution to study later

--- 07.14.22/nestingStructureComparison.js
+++ 07.14.22/nestingStructureComparison.js
@@ -46,3 +46,12 @@ Array.prototype.sameStructureAs = function (other) {
     };
     return matches(this, other);
 };
+
+
+// MUCH SIMPLER SOLUTION
+
+Array.prototype.sameStructureAs = function (other) {
+    return (this.length === other.length) ? this.every(function(el, i){
+      return Array.isArray(el) ? el.sameStructureAs(other[i]) : true;
+    }) : false;
+};
