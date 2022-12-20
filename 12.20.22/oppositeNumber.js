326ae618dbf7abcf306e4edbe75c523752201c26
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-12-20 12:13:23 -0500

    oppositeNumber solution

---
+++ 12.20.22/oppositeNumber.js
@@ -0,0 +1,12 @@
+// given integer
+
+// return opposite integer -1 => 1...4 => -4
+
+function opposite(number) {
+  return number * -1
+}
+
+// same but cleaner
+function opposite(number) {
+  return (-number)
+}
