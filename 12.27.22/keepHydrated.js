4ef4dc620b0e79785eb9436268425557ae097027
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-12-27 15:18:15 -0500

    keepHydrated solution

---
+++ 12.27.22/keepHydrated.js
@@ -0,0 +1,7 @@
+// given number representing amount of hours
+
+// return amount of liters that will be drunk at 0.5 per hour rounded to smallest value
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
